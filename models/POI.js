f3d7d14995e8a7cec3d374d26ac181ab525ba45d
justincrywolf <justincrywolf@gmail.com>
2018-04-27 11:29:50 -0400

    adding config.json and updating server.js

---
+++ models/POI.js
@@ -0,0 +1,12 @@
+module.exports = function (sequelize, DataTypes) {
+    var POI = sequelize.define("POI", {
+        title: DataTypes.STRING,
+        address: DataTypes.STRING,
+        lat: DataTypes.DECIMAL(9,6),
+        lng: DataTypes.DECIMAL(9,6),
+        link: DataTypes.STRING,
+        category: DataTypes.STRING,
+        body: DataType.TEXT
+    });
+    return POI;
+};
