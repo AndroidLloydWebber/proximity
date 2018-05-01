340bf15a8d5d0e285eada8574fa4b384292e5baa
AndroidLloydWebber <androidlloydwebber@github.com>
2018-05-01 10:47:28 -0400

    working version of map and new html

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
 DROP DATABASE IF EXISTS proximity;
 
 CREATE DATABASE proximity;
+
+CREATE TABLE events (
+d INT NOT NULL PRIMARY KEY,
+name VARCHAR(40),
+start VARCHAR(30),
+days INT NOT NULL,
+date_made DATE,
+rental_price FLOAT
+);
