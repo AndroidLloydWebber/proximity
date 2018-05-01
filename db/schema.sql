892323e35b231905a4a6e1c1a4238f04eab53e4c
AndroidLloydWebber <androidlloydwebber@github.com>
2018-05-01 19:20:28 -0400

    schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,13 @@ CREATE DATABASE proximity;
 
 
 CREATE TABLE POI (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255),
+    address VARCHAR(255),
+    lat DECIMAL(9,6),
+    lng DECIMAL(9,6),
+    link VARCHAR(255),
+    category VARCHAR(255),
+    body TEXT
     
 );
